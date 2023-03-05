a43b2ce29829bb9644906f3303af86e72c93c6d4
Casey Muratori <casey@mollyrocket.com>
2023-03-05 13:35:31 -0800

    Fixed the listing to test 8-bit immediate-to-register moves

--- perfaware/part1/listing_0039_more_movs.asm
+++ perfaware/part1/listing_0039_more_movs.asm
@@ -21,10 +21,12 @@ mov si, bx
 mov dh, al
 
 ; 8-bit immediate-to-register
-mov cx, 12
-mov cx, -12
+mov cl, 12
+mov ch, -12
 
 ; 16-bit immediate-to-register
+mov cx, 12
+mov cx, -12
 mov dx, 3948
 mov dx, -3948
 
