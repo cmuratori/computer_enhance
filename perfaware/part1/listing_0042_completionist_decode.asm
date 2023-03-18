f18630d6310d0fba5260a3025755cc09359f7cb1
Casey Muratori <casey@mollyrocket.com>
2023-03-18 16:09:52 -0700

    Added new test cases found by users

--- perfaware/part1/listing_0042_completionist_decode.asm
+++ perfaware/part1/listing_0042_completionist_decode.asm
@@ -410,11 +410,29 @@ lock not byte CS:[bp + 9905]
 
 ;
 ; NOTE(casey): These were not in the original homework set, but have been added since, as people
-; found instruction encodings that were not previously covered
+; found instruction encodings that were not previously covered. Thank you to everyone who
+; submitted test cases!
 ;
 
 call 123:456
 jmp 789:34
+
+mov [bx+si+59],es
+
+jmp 2620
+call 11804
+
+retf 17556
+ret 17560
+retf
+ret
+
+call [bp+si-0x3a]
+call far [bp+si-0x3a]
+jmp [di]
+jmp far [di]
+
+jmp 21862:30600
 
 ;
 ; TODO(casey): I would like to uncomment this, but as far as I can tell, NASM doesn't recognize the ESC instruction :(
