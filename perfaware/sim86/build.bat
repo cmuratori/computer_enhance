6be5531f6ac1382730d1cb4d8be4fd40de25a9b8
Casey Muratori <casey@mollyrocket.com>
2023-03-20 10:22:09 -0700

    Updated source files for new shared directory structure

--- perfaware/sim86/build.bat
+++ perfaware/sim86/build.bat
@@ -1,14 +1,22 @@
 @echo off
 IF NOT EXIST build mkdir build
 pushd build
+
 call cl -nologo -Zi -FC ..\sim86.cpp -Fesim86_msvc_debug.exe
 call clang -g -fuse-ld=lld ..\sim86.cpp -o sim86_clang_debug.exe
 call cl -O2 -nologo -Zi -FC ..\sim86.cpp -Fesim86_msvc_release.exe
 call clang -O3 -g -fuse-ld=lld ..\sim86.cpp -o sim86_clang_release.exe
 
-call clang -P -E ..\sim86_lib.h | call clang-format --style="Microsoft" > sim86_shared.h
+call clang -P -E ..\sim86_lib.h | call clang-format --style="Microsoft" > ..\shared\sim86_shared.h
 call clang -P -E ..\sim86_instruction_table_standalone.h | call clang-format --style="Microsoft" > sim86_instruction_table_standalone.h
-call cl -nologo -Zi -FC ..\sim86_lib.cpp -Fesim86_shared.dll /link /DLL /export:Sim86_Decode8086Instruction /export:Sim86_RegisterNameFromOperand /export:Sim86_MnemonicFromOperationType /export:Sim86_Get8086InstructionTable /export:Sim86_GetVersion
 
-call cl -nologo -Zi -FC ..\shared_library_test.cpp -Feshared_library_test.exe /link
+call cl -nologo -Zi -FC ..\sim86_lib.cpp -Fesim86_shared_debug.dll /link /DLL /PDBALTPATH:sim86_shared_debug.pdb /export:Sim86_Decode8086Instruction /export:Sim86_RegisterNameFromOperand /export:Sim86_MnemonicFromOperationType /export:Sim86_Get8086InstructionTable /export:Sim86_GetVersion
+call cl -nologo -O2 -Zi -FC ..\sim86_lib.cpp -Fesim86_shared_release.dll /link /DLL /PDBALTPATH:sim86_shared_release.pdb /export:Sim86_Decode8086Instruction /export:Sim86_RegisterNameFromOperand /export:Sim86_MnemonicFromOperationType /export:Sim86_Get8086InstructionTable /export:Sim86_GetVersion
+
+call copy sim86_shared*.dll ..\shared
+call copy sim86_shared*.lib ..\shared
+call copy sim86_shared*.pdb ..\shared
+
+call cl -nologo -Zi -FC ..\shared\shared_library_test.cpp -Feshared_library_test.exe /link
+
 popd
--- perfaware/sim86/sim86.h
+++ perfaware/sim86/sim86.h
@@ -10,8 +10,6 @@
    
    ======================================================================== */
 
-#define SIM86_VERSION 3
-
 typedef char unsigned u8;
 typedef short unsigned u16;
 typedef int unsigned u32;
@@ -25,3 +23,5 @@ typedef long long s64;
 typedef s32 b32;
 
 #define ArrayCount(Array) (sizeof(Array) / sizeof((Array)[0]))
+
+static u32 const SIM86_VERSION = 3;
