2dabab11252eafb2abe541f44a17fd8c83971534
Pierre-Henri Trivier <phtrivier@yahoo.fr>
2023-03-29 22:12:37 +0200

    Fixed memory access by passing mutable pointer properly

--- perfaware/sim86/shared/contrib_rust/src/main.rs
+++ perfaware/sim86/shared/contrib_rust/src/main.rs
@@ -4,8 +4,6 @@
 
 include!(concat!(env!("OUT_DIR"), "/sim86_lib_bindings.rs"));
 
-use std::mem;
-
 #[cfg(test)]
 mod test {
 
@@ -15,22 +13,24 @@ mod test {
     fn decode_register_to_register_mov() {
 
         let disassembly: [u8;2] = [
-            0b10001001, 0b11000100, // same in binary
+            0b10001001, 0b11000100, // mov sp,ax; in binary, unless I'm mistaken
 
             // Ob 100010 01 -> Opcode : Mov, D: false, W: true
             // 0b 11 000 100 -> Mod: 11, Reg: 000 R/M: 100 -> Reg AX, RM: SP
         ];
 
-        let decoded_instruction = unsafe {
-            let SourceSize = disassembly.len() as u32;
-            let mut Decoded: instruction = mem::zeroed();
+        let mut Decoded = std::mem::MaybeUninit::uninit();
+        let Source_ptr = disassembly.as_ptr() as *mut u8;
+        // let mut Source = disassembly[0];
 
-            let mut Source = disassembly[0];
+        let decoded_instruction : instruction = unsafe {
+            let SourceSize = disassembly.len() as u32;
 
             Sim86_Decode8086Instruction(SourceSize,
-                                        &mut Source,
-                                        &mut Decoded);
-            Decoded
+                                        // &mut Source,
+                                        Source_ptr,
+                                        Decoded.as_mut_ptr());
+            Decoded.assume_init()
         };
 
         assert_eq!(decoded_instruction.Op, operation_type_Op_mov);
@@ -43,7 +43,7 @@ mod test {
         assert_eq!(operand1_register_access.Offset, 0);
         assert_eq!(operand1_register_access.Count, 2);
 
-        // Operand 0 is not
+        // Operand 0 is not properly parsed, for some reason, it's considered as a Memory...
         let operand0 = decoded_instruction.Operands[0];
         assert_eq!(operand0.Type, operand_type_Operand_Register);
         let operand0_register_access = unsafe { operand0.__bindgen_anon_1.Register };
