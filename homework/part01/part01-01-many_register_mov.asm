13c2d9901fade72e342c7c389cef1f2ff95c30e1
Ole Vinsnes <Ole.Vinsnes@nhn.no>
2023-12-04 14:40:57 +0100

    Flyttet på filer

--- homework/part01/part01-01-many_register_mov.asm
+++ homework/part01/part01-01-many_register_mov.asm
@@ -2,7 +2,7 @@
 ; Here we are giving (encoding) CPU instructions on what to do.
 
 ; We want to be copying the contents from one 16 bit register to another.
-; The instruction that copy bits from one place to another, is called "mov".
+; The instruction that copies bits from one place to another is called "mov".
 ; To target all 16 bits we use the x-suffix.
 ; To only target the high or low bits, we could use the h- or l-suffixes.
 
